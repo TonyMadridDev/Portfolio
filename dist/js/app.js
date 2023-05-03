bdc8f9b810a0786390c57787a602d5bfd77ca3f4
TonyM <tonymadriddev@gmail.com>
2023-05-02 21:44:57 -0700

    Initial Commit

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('navbar-toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+
+
+// Toggle Nav
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
